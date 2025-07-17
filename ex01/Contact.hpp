c65a0c68b72ca73be3b68c96a78480c7b2511740
Rachel Neumann <raneuman@mi-r2-p2.s19.be>
2025-07-17 18:21:26 +0200

    test

--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -6,7 +6,7 @@
 /*   By: raneuman <raneuman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 17:32:23 by raneuman          #+#    #+#             */
-/*   Updated: 2025/07/17 18:20:38 by raneuman         ###   ########.fr       */
+/*   Updated: 2025/07/17 18:21:21 by raneuman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include <iostream>
 # include <iomanip>
 # include <cctype>//Pour isspace.
-#include <cstdlib>//Pour exit.
+# include <cstdlib>//Pour exit.
 
 class	Contact
 {
