577b5985df9d22b19049caac9c4e1bdeeb425fb1
Klaenex <klaenex.29@gmail.com>
2025-07-30 00:02:06 +0200

    test

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: raneuman <raneuman@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/15 17:03:23 by raneuman          #+#    #+#             */
-/*   Updated: 2025/07/18 17:32:48 by raneuman         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "PhoneBook.hpp"
 
 int	main(int argc, char **argv)
@@ -20,7 +8,7 @@ int	main(int argc, char **argv)
 	std::cout << "Welcome to the PhoneBook!" <<std::endl;
 	while (1)
 	{
-		std::cout << "Please enter one of thesecommands (ADD, SEARCH, EXIT): ";
+		std::cout << "Please enter one of these commands (ADD, SEARCH, EXIT): ";
 		if (!std::getline(std::cin, command))
 		{
 			std::cout << "\nInput closed. Exiting..."<< std::endl;
@@ -33,7 +21,7 @@ int	main(int argc, char **argv)
 		else if (command == "EXIT")
 			break ;
 		else
-			std::cout << "Unknown command. Please trywith ADD, SEARCH or EXIT." << std::endl;
+			std::cout << "Unknown command. Please try with ADD, SEARCH or EXIT." << std::endl;
 	}
 	std::cout << "Goodbye!" << std::endl;
 
